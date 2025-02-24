e6725be7c36ee6b487037c1e855743b96f485d8f
Saladin Afoh <saladuit@gmail.com>
2025-02-24 23:35:16 +0100

    initial flappy bird on blue screen

---
+++ README.md
@@ -0,0 +1,3 @@
+compiling for windows:
+ - use mvsc
+ - `CARGO_FEATURE_PURE=1 cargo build --target x86_64-pc-windows-msvc`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+
+const PIXEL_RATIO: f32 = 4.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: String::from("Flappy bird"),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: Vec2::new(512., 512.).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup_level)
+        .run();
+}
+
+#[derive(Component)]
+struct Bird {
+    pub velocity: f32,
+}
+
+fn setup_level(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(ClearColor(Color::srgb(0.5, 0.7, 0.8)));
+    commands.spawn(Camera2d::default());
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("bird.png"),
+            ..Default::default()
+        },
+        Transform::IDENTITY.with_scale(Vec3::splat(PIXEL_RATIO)),
+        Bird { velocity: 0.0 },
+    ));
 }
